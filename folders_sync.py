221f8fa753b7d64583ae738eab1f5266a9ab6aee
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2025-02-12 13:47:48 +0000

    20250212

--- folders_sync.py
+++ folders_sync.py
@@ -9,9 +9,22 @@ def run_program() -> None:
     if not source_folder_path:
         return
     
-    # stooped here!
-    source_replica_folders_path = get_replica_folder(source_folder_path)
-    get_sync_interval()
+    replica_folder_path = get_replica_folder(source_folder_path)
+    
+    if not replica_folder_path:
+        return
+    
+    log_file_path = get_log_folder(source_folder_path, replica_folder_path)
+    
+    if not log_file_path:
+        return
+    
+    sync_interval = get_sync_interval()
+    
+    if not sync_interval:
+        return
+    
+    
     return
 
 
@@ -43,7 +56,7 @@ def get_source_folder() -> str | None:
     
     while True:
         
-        source_folder_path = input("\n\033[32mEnter the Source Folder Path: \033[0m").strip()
+        source_folder_path = input("\n\033[32mEnter the \033[0mSource Folder\033[32m Path: \033[0m").strip()
         
         if verify_folder_path(source_folder_path):
             return source_folder_path        
@@ -57,14 +70,14 @@ def get_source_folder() -> str | None:
                 
         
     
-def get_replica_folder(source_folder_path: str) -> tuple[str, str] | None:
+def get_replica_folder(source_folder_path: str) -> str | None:
     
     while True:
         
-        replica_folder_path = input("\n\033[32mEnter the Replica Folder Path: \033[0m").strip()
+        replica_folder_path = input("\n\033[32mEnter the \033[0mReplica Folder\033[32m Path: \033[0m").strip()
         
         if replica_folder_path != source_folder_path and verify_folder_path(replica_folder_path):
-            return source_folder_path, replica_folder_path
+            return replica_folder_path
         else:
             print("\033[32mReplica Folder path same as Source Folder, inexistent or invalid.")
             if not get_user_choice():
@@ -76,17 +89,28 @@ def get_replica_folder(source_folder_path: str) -> tuple[str, str] | None:
         
     
     
-def get_log_folder(source_replica_paths: tuple[str, str]) -> str | None:
-    
-    source_folder_path, replica_folder_path = source_replica_paths
-    
+def get_log_folder(source_folder_path: str, replica_folder_path: str) -> str | None:
+     
     while True:
         
-        log_file_path = input("\n\033[32mEnter the Replica Folder Path: \033[0m").strip()
+        log_file_path = input("\n\033[32mEnter the \033[0mLog File\033[32m Path: \033[0m").strip()
+        
+        if not log_file_path:
+            print("\033[32mLog File folder path same as Source/Replica Folder, inexistent or invalid.")
+            if not get_user_choice():
+                return None
+            else:
+                clean_console()
+                show_instructions()
+                continue
         
         if log_file_path != source_folder_path and log_file_path != replica_folder_path:
             if verify_folder_path(log_file_path):
                 return log_file_path
+            else:
+                clean_console()
+                show_instructions()
+                continue
         else:
             print("\033[32mLog File folder path same as Source/Replica Folder, inexistent or invalid.")
             if not get_user_choice():
@@ -109,9 +133,62 @@ def verify_folder_path(folder_path: str) -> bool:
     
     
     
-def get_sync_interval():
+def get_sync_interval() -> int | None:
     
-    sync_interval = input("\n\033[32mEnter the Synchronization Interval: ").strip()
+    allowed_interval = { "m": 59, "h": 23, "d": 30}
+    
+    while True:
+        
+        sync_interval = input("\n\033[32mEnter the \033[0mSynchronization Interval\033[32m: \033[0m").strip().lower()
+        
+        if len(sync_interval) != 2:
+            print("\033[32mInvalid format. Enter one number followed by m (minutes), h (hours), or d (days).")
+            if not get_user_choice():
+                return None
+            else:
+                clean_console()
+                show_instructions()
+                continue
+            
+        time = sync_interval[:-1]
+        unit = sync_interval[-1]
+        
+        if not time.isdigit() or unit not in allowed_interval:
+            print("\033[32mInvalid format. Examples: 5m, 1h, 2d")
+            if not get_user_choice():
+                return None
+            else:
+                clean_console()
+                show_instructions()
+                continue
+            
+        time = int(time)
+        
+        if time <= 0:
+            print("\033[32mNumber must be greater than 0.")
+            clean_console()
+            show_instructions()
+            continue
+        
+        if time > allowed_interval[unit]:
+            print(f"\033[32mMaximum allowed value for {unit} is {allowed_interval[unit]}.")
+            clean_console()
+            show_instructions()
+            continue
+        break
+    
+    return transform_sync_interval(unit, time)
+    
+    
+
+def transform_sync_interval(unit: str, time: int) -> int:
+    
+    if unit == "m":
+        return time * 60
+    elif unit == "h":
+        return time * 3600
+    else:
+        return time * 86400
 
 
 
@@ -133,7 +210,7 @@ def show_message(message: str) -> None:
         case "welcome":
             print(f"\033[32m\n{separator}\n{title.center(len(separator))}\n{separator}\n\033[0m")
         case "instructions":
-            print("\033[32mYou will be asked to enter:\n\n\t--> the Source Folder Path, ex:\n\t\t/source_path/source_path/source_folder_name\n\n\t--> the Replica Folder Path, ex:\n\t\t/replica_path/replica_path/replica_folder_name\n\n\t--> the Log File Path, ex:\n\t\t/log_path/log_path/log_file_path_name\n\n\t--> the Synchronization Interval Time or Day(s) with one (1) digit and one (1) letter:\n\t\t(h) for hours\n\t\t(d) for days\n\tex:\n\t\t1h (1 hour) \n\t\t1d (1 day)\033[0m")
+            print("\033[32mYou will be asked to enter:\n\n\t--> the Source Folder Path, ex:\n\t\t/source_path/source_path/source_folder_name\n\n\t--> the Replica Folder Path, ex:\n\t\t/replica_path/replica_path/replica_folder_name\n\n\t--> the Log File Path, ex:\n\t\t/log_path/log_path/log_file_path_name\n\n\t--> the Synchronization Interval Minute(s), Hour(s) or Day(s) with one (1) digit and one (1) letter:\n\t\t(m) for minutes\n\t\t(h) for hours\n\t\t(d) for days\n\tex:\n\t\t10m (10 minutes) \n\t\t1h (1 hour) \n\t\t1d (1 day)\033[0m")
         case "extensions":
             print("\033[32m--> The valid extensions are: ")
         
